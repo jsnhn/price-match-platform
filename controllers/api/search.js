2cf9d307973272fb985d98ae2cfb98bb4bcf9649
jsnhn <jayhh122@gmail.com>
2024-09-24 13:39:25 -0700

    integrates serpAPI in controller

--- controllers/api/search.js
+++ controllers/api/search.js
@@ -2,8 +2,28 @@ module.exports = {
     index
 };
 
+const { getJson } = require('serpapi')
+
 async function index(req, res) {
-    const searchTerm = req.body.query; // Access the query parameter
-    console.log(searchTerm); // Log to see if it’s coming through
-    res.json(searchTerm)
+    const searchTerm = req.body.query; 
+    
+    try {
+        getJson({
+            engine:'google',
+            q: searchTerm,
+            hl: 'en',
+            gl: 'us',
+            google_domain: 'google.com',
+            num: '5',
+            safe:'active',
+            api_key: process.env.SERPAPI
+        }, (json) => {
+            res.json(searchTerm)
+            console.log(json["organic_results"]);
+        });
+
+    } catch (err) {
+        console.error
+        res.status(400).json(err);
+    }
 }
