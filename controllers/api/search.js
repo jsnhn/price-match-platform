c48082b46fbcdb572383ac174f04828901c5d9e1
jsnhn <jayhh122@gmail.com>
2024-12-27 12:52:29 -0800

    adjusting search bar

--- controllers/api/search.js
+++ controllers/api/search.js
@@ -12,21 +12,21 @@ async function index(req, res) {
     const searchTerm = req.body.query; 
     
     try {
-        // getJson({
-        //     engine:'google_shopping',
-        //     q: searchTerm,
-        //     hl: 'en',
-        //     gl: 'us',
-        //     google_domain: 'google.com',
-        //     num: '5',
-        //     safe:'active',
-        //     direct_link: true,
-        //     api_key: process.env.SERPAPI
-        // }, (json) => {
-        //     writeObjToFile(json);
-        //     res.json(json.shopping_results);
-        // });
-        res.json(sampleData.shopping_results)
+        getJson({
+            engine:'google_shopping',
+            q: searchTerm,
+            hl: 'en',
+            gl: 'us',
+            google_domain: 'google.com',
+            num: '5',
+            safe:'active',
+            direct_link: true,
+            api_key: process.env.SERPAPI
+        }, (json) => {
+            // writeObjToFile(json);
+            res.json(json.shopping_results);
+        });
+        // res.json(sampleData.shopping_results)
     } catch (err) {
   // Correctly log the error
         console.error("Error occurred:", err);
